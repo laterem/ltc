b2942dd7b3e4c00e3c7d171d471cecfd52730ef9
Sp1dz <spidepog@gmail.com>
2023-03-07 20:09:48 +0300

    ~ Fixed module imports

--- ltc.py
+++ ltc.py
@@ -1,9 +1,9 @@
-from src.ltc_core import LTCError
-from src.ltc_core import LTCCompileError
-from src.ltc_core import LTCExecutionError
-from src.ltc_core import register_function
+from .src.ltc_core import LTCError
+from .src.ltc_core import LTCCompileError
+from .src.ltc_core import LTCExecutionError
+from .src.ltc_core import register_function
 
-from src.ltc_compiler import LTCCompiler
-from src.ltc_compiler import LTC
-from src.ltc_compiler import LTCMetadataManager
-from src.ltc_compiler import LTCFakeMetadata
+from .src.ltc_compiler import LTCCompiler
+from .src.ltc_compiler import LTC
+from .src.ltc_compiler import LTCMetadataManager
+from .src.ltc_compiler import LTCFakeMetadata
